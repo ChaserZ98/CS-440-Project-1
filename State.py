c9c42664baa0db9e18d0dc5715d80a80dfcf2bff
Feiyu Zheng <feiyuzheng98@gmail.com>
2020-07-03 00:33:15 -0400

    create State.py class

---
+++ State.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+class State:
+    def __init__(self, x, y, isBlocked):
+        self.location = np.array([x, y])
+        self.isBlocked = isBlocked
+        self.searchValue = 0
+        self.gValue = 0
+        self.hValue = 0
+        self.fValue = self.gValue + self.hValue
+
+    def updateFValue(self):
+        self.fValue = self.gValue + self.hValue
+

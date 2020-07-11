70a50a61bd6b930ec21525209c0a9f83b5adab2a
Feiyu Zheng <feiyuzheng98@gmail.com>
2020-07-11 19:06:18 -0400

    add actualBlockStatus and discoveredBlockStatus. actualBlockStatus records the actual block status of the maze. discoverBlockStatus records the block status after agent check it.

--- State.py
+++ State.py
@@ -2,9 +2,10 @@ import numpy as np
 
 
 class State:
-    def __init__(self, x, y, isBlocked):
+    def __init__(self, x, y, actualBlockedStatus):
         self.location = np.array([x, y])
-        self.isBlocked = isBlocked
+        self.actualBlockStatus = actualBlockedStatus
+        self.discoveredBlockStatus = 0
         self.searchValue = 0
         self.gValue = 0
         self.hValue = 0

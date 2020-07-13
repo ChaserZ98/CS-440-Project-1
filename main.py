64f92fda23afd169ed6fd01afc3ebd122aa3b6f6
Feiyu Zheng <feiyuzheng98@gmail.com>
2020-07-12 21:37:17 -0400

    Add comment

--- main.py
+++ main.py
@@ -4,16 +4,31 @@ import repeatedAdaptiveAStar as adaptiveAStar
 import commonFunctions
 
 if __name__ == '__main__':
+
+    # Initialize states from grid world
     print("Initializing states...", end="")
-    states = commonFunctions.generateStates()  # initialize states
+    states = commonFunctions.generateStates()
     print("done!")
+
+    # Initialize start location and goal location
     print("Generating start location and goal location...", end="")
     startLocation = commonFunctions.generateUnblockedLocation(states)
     goalLocation = commonFunctions.generateUnblockedLocation(states)
     while (startLocation == goalLocation).all():
         goalLocation = commonFunctions.generateUnblockedLocation(states)
     print("done!")
+
+    # Decide the type of tie breaker
     isLargerGFirst = False
+
+    # A Star Search
+    print("Repeated Forward A Star: ")
     forwardAStar.repeatedForwardAStar(states, startLocation, goalLocation, isLargerGFirst)
+
+    states = commonFunctions.generateStates()   # Reset the states
+    print("Repeated Backward A Star: ")
     backwardAStar.repeatedBackwardAStar(states, startLocation, goalLocation, isLargerGFirst)
+
+    states = commonFunctions.generateStates()   # Reset the states
+    print("Repeated Adaptive A Star: ")
     adaptiveAStar.repeatedAdaptiveAStar(states, startLocation, goalLocation, isLargerGFirst)

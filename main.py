682115e9e17bdccf5b51628c38534353923ea0b8
Feiyu Zheng <feiyuzheng98@gmail.com>
2020-07-12 21:23:57 -0400

    Tested all three A star search

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import repeatedForwardAStar as forwardAStar
 import repeatedBackwardAStar as backwardAStar
 import repeatedAdaptiveAStar as adaptiveAStar
+import commonFunctions
 
 if __name__ == '__main__':
+    print("Initializing states...", end="")
+    states = commonFunctions.generateStates()  # initialize states
+    print("done!")
+    print("Generating start location and goal location...", end="")
+    startLocation = commonFunctions.generateUnblockedLocation(states)
+    goalLocation = commonFunctions.generateUnblockedLocation(states)
+    while (startLocation == goalLocation).all():
+        goalLocation = commonFunctions.generateUnblockedLocation(states)
+    print("done!")
     isLargerGFirst = False
-    forwardAStar.repeatedForwardAStar(isLargerGFirst)
-    backwardAStar.repeatedBackwardAStar(isLargerGFirst)
-    adaptiveAStar.repeatedAdaptiveAStar(isLargerGFirst)
+    forwardAStar.repeatedForwardAStar(states, startLocation, goalLocation, isLargerGFirst)
+    backwardAStar.repeatedBackwardAStar(states, startLocation, goalLocation, isLargerGFirst)
+    adaptiveAStar.repeatedAdaptiveAStar(states, startLocation, goalLocation, isLargerGFirst)

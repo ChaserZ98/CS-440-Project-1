a6eef2b31bd10cbe873da31478dfa0f9378b0e71
Feiyu Zheng <feiyuzheng98@gmail.com>
2020-07-14 03:21:50 -0400

    Add the details of the memory size

--- State.py
+++ State.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 
+# One numpy array (4 bytes), two boolean (2 bytes), four int (16 bytes), one pointer (8 bytes)
+# Total Size: 30 bytes
 class State:
     def __init__(self, x, y, actualBlockedStatus: bool):
         self.location = np.array([x, y])
